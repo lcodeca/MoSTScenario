01c1879ec913ae9219e363c9168b1912473ae2cc
Lara CODECA <lara.codeca@eurecom.fr>
2018-09-10 16:49:55 +0200

    added section intermodalOptions with parkingAreaWhitelist and vehicleAllowedParking

--- tools/mobility/intermodal.mobilitygen.py
+++ tools/mobility/intermodal.mobilitygen.py
@@ -166,7 +166,7 @@ class MobilityGenerator(object):
         xml_tree = xml.etree.ElementTree.parse(filename).getroot()
         for child in xml_tree:
             if (child.tag == 'parkingArea' and
-                    child.attrib['id'] in self._conf['SUMOadditionals']['parkingAreaWhitelist']):
+                    child.attrib['id'] in self._conf['intermodalOptions']['parkingAreaWhitelist']):
                 edge = child.attrib['lane'].split('_')[0]
                 position = float(child.attrib['startPos']) + 2.5
                 self._sumo_parkings[edge].append(child.attrib['id'])
@@ -358,7 +358,7 @@ class MobilityGenerator(object):
         for p_edge, parkings in self._sumo_parkings.items():
             _is_allowed = False
             for parking in parkings:
-                if parking in self._conf['SUMOadditionals']['parkingAreaWhitelist']:
+                if parking in self._conf['intermodalOptions']['parkingAreaWhitelist']:
                     _is_allowed = True
                     break
             if not _is_allowed:
@@ -455,7 +455,7 @@ class MobilityGenerator(object):
             _last_mile = None
             _modes, _ptype, _vtype = self._get_mode_parameters(mode)
 
-            if with_pl and _vtype in ['passenger', 'ptw', 'e-vehicle']:
+            if with_pl and _vtype in self._conf['intermodalOptions']['vehicleAllowedParking']:
                 ## Find the closest parking area
                 p_edge, _last_mile = self._find_closest_parking(to_edge)
                 if _last_mile:
