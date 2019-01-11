c4e159bba0264b6432450f2b14792fbc1d66b56b
Lara CODECA <lara.codeca@eurecom.fr>
2019-01-11 13:15:16 +0100

    regenerated poly weights and tazs

--- tools/mobility/mobility.generator.sh
+++ tools/mobility/mobility.generator.sh
@@ -33,16 +33,16 @@ mkdir -p $OUTPUT
 INTERVAL="-b 14400 -e 50400"
 # INTERVAL="-b 0 -e 7200"
 
-echo "Generate bus trips..."
-python $SUMO_DEV_TOOLS/ptlines2flows.py -n $INPUT/most.net.xml $INTERVAL -p 1200 \
+echo "[$(date)] --> Generate bus trips..."
+python $SUMO_TOOLS/ptlines2flows.py -n $INPUT/most.net.xml $INTERVAL -p 1200 \
     --random-begin --seed 42 --no-vtypes \
     --ptstops $ADD/most.busstops.add.xml --ptlines pt/most.buslines.add.xml \
     -o $OUTPUT/most.buses.flows.xml
 
 sed -e s/:0//g -i $OUTPUT/most.buses.flows.xml
 
-echo "Generate train trips..."
-python $SUMO_DEV_TOOLS/ptlines2flows.py -n $INPUT/most.net.xml $INTERVAL -p 1200 \
+echo "[$(date)] --> Generate train trips..."
+python $SUMO_TOOLS/ptlines2flows.py -n $INPUT/most.net.xml $INTERVAL -p 1200 \
     -d 300 --random-begin --seed 42 --no-vtypes \
     --ptstops $ADD/most.trainstops.add.xml --ptlines pt/most.trainlines.add.xml \
     -o $OUTPUT/most.trains.flows.xml
@@ -51,5 +51,5 @@ sed -e s/:0//g -i $OUTPUT/most.trains.flows.xml
 
 ## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ TRACI MOBILITY GENERATION ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ##
 
-echo "Generate mobility..."
+echo "[$(date)] --> Generate mobility..."
 python3 intermodal.mobilitygen.py -c most.intermodal.mobilitygen.json
